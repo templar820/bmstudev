19534683792fe866d767aa1b34336e440f8461f6
Иван Кузнецов <ttemplar820@gmail.com>
2020-05-10 17:32:51 +0300

    make it better

---
+++ build/precache-manifest.3bc8fd917e3d1c4daeb434816c0a98d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8b45e5f1cb00d1bea1c764c2b3a6f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7609ba509006b5d131ce",
+    "url": "/static/css/main.70d89298.chunk.css"
+  },
+  {
+    "revision": "9b7e2ce5bedb2b808319",
+    "url": "/static/js/2.8793df52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8793df52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7609ba509006b5d131ce",
+    "url": "/static/js/main.31dc685b.chunk.js"
+  },
+  {
+    "revision": "82e58d1683036018e5dd",
+    "url": "/static/js/runtime-main.e1d9e56a.js"
+  }
+]);
